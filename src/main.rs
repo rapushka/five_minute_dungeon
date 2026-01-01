00c08333d55a83a9bae95f74279bf32a26f907a2
rapuska <mikalajsiatkouski@gmail.com>
2026-01-01 23:47:32 +0300

    spawn player

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
-use bevy::DefaultPlugins;
-use bevy::prelude::App;
+use avian2d::prelude::*;
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(PhysicsPlugins::default())
+
+        .add_systems(Startup, setup)
+
         .run();
+}
+
+#[derive(Component)]
+struct Player;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+
+    let player_shape = meshes.add(Capsule2d::new(25.0, 50.0));
+    let player_material = materials.add(Color::srgba(1.0, 0.25, 0.25, 1.0));
+
+    commands.spawn(Player)
+        .insert(Mesh2d(player_shape))
+        .insert(MeshMaterial2d(player_material))
+    ;
 }
